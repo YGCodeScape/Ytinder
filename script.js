9e99962a83c8345cbf12ea30b3667dfbd8cb108c
yashG <yrg.creation.ai@gmail.com>
2025-08-23 23:46:01 +0530

    working on js

---
+++ script.js
@@ -0,0 +1,13 @@
+// get the data
+var users = [
+    {
+        profilePic: "",
+        pendingMessage: 5,
+        location: "Mumbai, India",
+        name: "Sayli",
+        age: 21,
+        interests: ["Music", "Traveling", "Study"],
+        bio: "Love to explore new places and enjoy good music.",
+        isFriends: null
+    }
+]
