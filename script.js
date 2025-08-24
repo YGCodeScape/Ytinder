8d9e93501d4df04613dc368c3f0423f274fd1981
yashG <yrg.creation.ai@gmail.com>
2025-08-24 21:27:20 +0530

    aded profiles data

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// get the data
+// get the data. add more girl user data
 var users = [
     {
-        profilePic: "",
+        profilePic: "https://i.pinimg.com/736x/e7/6f/86/e76f86a7889232e98e2c6533d11f1c43.jpg",
+        DisplayPic: "https://i.pinimg.com/736x/e7/6f/86/e76f86a7889232e98e2c6533d11f1c43.jpg",
+        pendingMessage: 3,
+        location: "USA, California",
+        name: "Jane",
+        age: 21,
+        interests: ["Music", "Reading", "Movies"],
+        bio: "In a committed relationship with headphones, the cinema, and the next chapter of a good book.",
+        isFrineds: null
+    },
+    {
+        profilePic: "https://i.pinimg.com/736x/7d/32/f9/7d32f9666082a3853f8f196c6c9ebb50.jpg",
+        DisplayPic: "https://i.pinimg.com/736x/89/dc/04/89dc04bde7c8425af40df77e3a8042fb.jpg",
         pendingMessage: 5,
         location: "Mumbai, India",
         name: "Sayli",
@@ -9,5 +21,53 @@ var users = [
         interests: ["Music", "Traveling", "Study"],
         bio: "Love to explore new places and enjoy good music.",
         isFriends: null
+    },
+    {
+        profilePic: "https://i.pinimg.com/736x/24/11/23/2411236492a3b72e153b91f7c60470ba.jpg",
+        DisplayPic: "https://i.pinimg.com/736x/4d/ce/8b/4dce8bfb3935de3275787b21226c5aaf.jpg",
+        pendingMessage: 2,
+        location: "Delhi, India",
+        name: "Ananya",
+        age: 22,
+        interests: ["Dancing", "Reading", "Cooking"],
+        bio: "Foodie and a dance enthusiast. Let's groove together!",
+        isFriends: null
+    },
+    {
+        profilePic: "https://i.pinimg.com/736x/6f/da/f7/6fdaf793b3e7b4857d005f0370e4d369.jpg",
+        DisplayPic: "https://i.pinimg.com/1200x/19/25/bf/1925bfebe812da1ad1c9b76cae320a95.jpg",
+        pendingMessage: 3,
+        location: "Bangalore, India",
+        name: "Priya",
+        age: 23,
+        interests: ["Traveling", "Fitness"],
+        bio: "Capturing moments and staying fit. Let's connect!",
+        isFriends: null
+    },
+    {
+        profilePic: "https://i.pinimg.com/736x/b9/17/4c/b9174ce6ec4f5f20ab3714ff19c2e4e1.jpg",
+        DisplayPic : "https://i.pinimg.com/736x/4b/a2/a0/4ba2a0286fc49124472da74c7585b59f.jpg",
+        pendingMessage: 4,
+        DisplayPic: "",
+        location: "Chennai, India",
+        name: "Ankita",
+        age: 24,
+        interests: ["Cooking", "Traveling"],
+        bio: "A culinary enthusiast who loves to travel and practice yoga.",
+        isFriends: null
     }
-]
+];
+
+var crr = 0;  // current profile 
+
+(function setInitial() {
+    document.querySelector(".maincard img").src = users[crr].DisplayPic;
+    document.querySelector(".pro-img img").src = users[crr].profilePic;
+    document.querySelector(".badge h5").textContent = users[crr].pendingMessage;
+    document.querySelector(".location h3").textContent = users[crr].location;
+    document.querySelector(".name-age h1:nth-child(1)").textContent = users[crr].name;
+    document.querySelector(".name-age h1:nth-child(2)").textContent = users[crr].age;
+
+    document.querySelector(".incomingcard img").src = users[crr+1].DisplayPic;
+    crr = 2;
+})();
